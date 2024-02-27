1bb59e7bb1408f0583fe09f22e3182b104d7f7a0
thaleseqb <thalesquadros2@gmail.com>
2024-02-27 14:52:06 -0300

    improving function that reorders an array and creating a new testing document

--- programing_logic.js
+++ programing_logic.js
@@ -4,6 +4,15 @@ price_list = [25, 15, 30, 20, 45, 50];
 // someone needs to ordenate the list in te crescent order
 
 // it is necessary to use spread operator to get the minimum value from an array
-// console.log(Math.min(...price_list))
+
+function ordenateArray(price_list) {
+    for (let idx = 0; idx < price_list.length; idx++){
+        let min_value = Math.min(price_list);
+        let index = price_list.indexOf(min_value)
+    }
+};
+
+
+
 
 
---
+++ testing.js
@@ -0,0 +1,10 @@
+book_list = ["Javascript", "PHP", "Java", "Python", "Go", "elixir"];
+price_list = [25, 15, 30, 20, 45, 50];
+
+// let min_value = Math.min(...price_list)
+// let boolean_value = price_list.includes(min_value)
+
+// const value = boolean_value ? price_list.indexOf(min_value) : 'nao ta certo'
+
+// console.log(value)
+

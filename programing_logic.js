cfec426cd5fb2bac849f029c7aa24450db0413f2
thaleseqb <thalesquadros2@gmail.com>
2024-02-27 14:34:42 -0300

    some anotations related with the math.min and spread operator

---
+++ programing_logic.js
@@ -0,0 +1,9 @@
+book_list = ["Javascript", "PHP", "Java", "Python", "Go", "elixir"];
+price_list = [25, 15, 30, 20, 45, 50];
+
+// someone needs to ordenate the list in te crescent order
+
+// it is necessary to use spread operator to get the minimum value from an array
+// console.log(Math.min(...price_list))
+
+
